f5ab6768a592dcd57f1c3ce38193ae0e74615e2a
piotrczupajlo <piotrczupajlobusiness@wp.pl>
2026-02-20 10:11:00 +0100

    Initial commit

---
+++ testapi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testapi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
